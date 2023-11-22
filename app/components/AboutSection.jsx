1fc43e75cbbdae2b9b0ce2d7a652359a00165a6c
SeniboDonPedro <senibodonpedro@gmail.com>
2023-11-22 09:30:29 +0100

    error with alt tags

--- app/components/AboutSection.jsx
+++ app/components/AboutSection.jsx
@@ -62,7 +62,12 @@ const AboutSection = () => {
   return (
     <section className="text-white">
       <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
-        <Image src="/images/coding-gif.gif" width={600} height={600} />
+        <Image
+          src="/images/coding-gif.gif"
+          alt="decoration"
+          width={600}
+          height={600}
+        />
         <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
           <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
           <p className="text-base ">
